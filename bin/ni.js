8189b9830dddedc7d6393a59f02d69b619c2160d
imdevin567 <devin@devinyoungweb.com>
2013-01-18 14:05:44 -0600

    fixed cli path

--- bin/ni.js
+++ bin/ni.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 var program = require('commander')
-, nodeigniter = require('../');
+, nodeigniter = require('../lib/cli/');
 
 program.version(nodeigniter.version);
 
 program.command('create')
 .description('-> create Nodeigniter app')
 .action(function() {
-	nodeigniter.cli.create(program.args.shift() || '.');
+	nodeigniter.create(program.args.shift() || '.');
 });
